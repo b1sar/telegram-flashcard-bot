beef3a08e80e3e239e7493a73f59b4589b5bce51
b1sar <cebrailylmz02@gmail.com>
2020-06-30 07:44:52 +0300

    Remove unnecessary @ ComponentScan annotation

--- src/main/java/com/cebrail/botum/BotumApplication.java
+++ src/main/java/com/cebrail/botum/BotumApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
-@ComponentScan
 public class BotumApplication {
 
 	public static void main(String[] args) {
